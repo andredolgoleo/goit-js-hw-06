6c59c1e299923a1f6e9818bdd90ed7a17d581f9c
Andrey <andrej.dolgov.042110@gmail.com>
2022-10-14 02:41:37 +0300

    fixed some things

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', e => {
   const item = e.target;
 
-  if (item.value.length < 6) {
+  if (item.value.length < item.getAttribute('data-length')) {
     input.className = 'invalid';
     return;
   };
